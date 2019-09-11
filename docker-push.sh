95f1709b85b1b402ccb9721ebe2e8065b62a7a62
Walery Dewelow <walery.dewelow@wp.pl>
2019-09-11 15:27:58 +0200

    ci changes

--- docker-push.sh
+++ docker-push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker tag $TRAVIS_REPO_SLUG:travis $TRAVIS_REPO_SLUG
 docker push $TRAVIS_REPO_SLUG:travis
